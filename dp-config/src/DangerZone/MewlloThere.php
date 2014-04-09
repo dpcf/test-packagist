b191feda19f6a85e2a21d4ef422a2a8c9d7b075c
fdubois <frederick.dubois@duproprio.com>
2014-04-09 15:25:50 -0400

    test adding a package as a subfolder

---
+++ dp-config/src/DangerZone/MewlloThere.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Dpcf\DangerZone;
+
+/**
+ * A terribly useful class
+ *
+ * See: http://knpuniversity.com/screencast/question-answer-day
+ */
+class MewlloThere
+{
+    public function sayMewllo()
+    {
+        return 'Mewllo! Version 01';
+    }
+}
